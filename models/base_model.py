77bcc5648e359b9c22ef6a94c973c5ba4d43faac
Hedgebzh <axel.gore35@gmail.com>
2022-10-11 15:36:07 +0200

    fix id attribute

--- models/base_model.py
+++ models/base_model.py
@@ -10,21 +10,26 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
 
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "my_number":
-                    self.my_number = value
-                if key == "created_at":
+                    self.my_number = int(value)
+                elif key == "created_at":
                     self.created_at = datetime.strptime(value,
                     "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "updated_at":
+                elif key == "id":
+                    self.id = value
+                elif key == "updated_at":
                     self.updated_at = datetime.strptime(value,
                     "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "name":
-                    self.name = value
+                elif key == "name":
+                    self.name = str(value)
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
@@ -34,6 +39,7 @@ class BaseModel:
 
     def to_dict(self):
         dictionary = self.__dict__.copy()
+        print(self.__dict__)
         dictionary["__class__"] = str(__class__.__name__)
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
