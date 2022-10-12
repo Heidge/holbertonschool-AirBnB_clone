75a425133a2e781642d85fd49b61fe01b4dac13e
Hedgebzh <axel.gore35@gmail.com>
2022-10-12 17:15:57 +0200

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -26,6 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new()
 
 
 
@@ -34,7 +35,6 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.utcnow()
-        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,13 +3,15 @@
     Module FileStorage class
 """
 import json
+from models import base_model
+import os
 
 
 class FileStorage():
     """Class that serializes instances to a JSON file and deserializes JSON
     file to instances"""
 
-    __file_path = 'file.json'
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
@@ -21,5 +23,15 @@ class FileStorage():
 
     def save(self):
         dictionary = {}
+        for key, value in self.__objects.items():
+            dictionary[key] = value.to_dict()
         with open(self.__file_path, "w", encoding="utf-8") as f:
             json.dump(dictionary, f)
+
+    def reload(self):
+        """ deserializes JSON to __objects if JSON file (__file_path) """
+        if os.path.exists(self.__file_path) is True:
+            with open(self.__file_path, 'r', encoding='utf-8') as f:
+                objects_dict = json.load(f)
+                for key, value in objects_dict.items():
+                    self.__objects[key] = eval(value['__class__'])(**value)
